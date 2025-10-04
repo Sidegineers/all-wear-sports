58436509668bcde151f6c8231c0cfa24db315116
Jedi Code - Maverick <16720054+thejedicode09@users.noreply.github.com>
2025-10-04 13:29:04 +0100

    Rb

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/all-wear-sports/", // Must match your repository name
+export default defineConfig(({ mode }) => ({
+  base: "/all-wear-sports/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
-  }
-});
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean)
+  ,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
